ee031783cbe6c9496abd3921efbb998702002d69
KevinJeon <KevinJeon@users.noreply.github.com>
2026-03-28 10:31:23 +0000

    deploy: 02f1008c12f3920ba3cd076d5204b2747a8c5cb2

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f4073e00.js',
+                path_to_root + 'searchindex-964f3250.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
