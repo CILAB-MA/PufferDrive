bf228f3fa16aeb86a84a170677c2c121275b1b94
kevinjeon <kevinjeon119@gmail.com>
2026-08-08 02:15:49 +0000

    Delete SAE

--- analyze/sae/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Sparse autoencoders over Drive partner-encoder representations."""
-
-from .sae_model import SAEConfig, SparseAutoencoder, TopK, build_sae
-
-__all__ = ["SAEConfig", "SparseAutoencoder", "TopK", "build_sae"]
-
-# Collect entrypoint lives in collect_sae_activations.py (heavy LP deps).
